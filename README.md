e8cd4a476f3399886f7099478789f34d8ff24a44
Joel Carranza <jec@gatekeeper.com>
2021-02-25 17:45:23 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# todoist-project-folders
+
+Maintain individual folders for each Todoist project
+
+# To run for development
+
+    $ python -m tdpf:cli
+# Installation
+
+Simply run:
+
+    $ pipx install .
+
+
+# Usage
+
+To use it:
+
+    $ td-project-folders --help
+
---
+++ setup.py
@@ -0,0 +1,48 @@
+"""
+Maintain individual folders for each Todoist project
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click', 'todoist-python']
+
+setup(
+    name='todoist-project-folders',
+    version='0.1.0',
+    url='https://github.com/joelcarranza/todoist-project-folders',
+    license='BSD',
+    author='Joel Carranza',
+    author_email='joel@joelcarranza.com',
+    description='Maintain individual folders for each Todoist project',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'td-project-folders = tdpf.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
